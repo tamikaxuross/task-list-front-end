efed3d3809147883f77389d22198bfa3f0fcd210
Vicky Farinango <vickyfarinango@gmail.com>
2025-06-10 13:38:14 -0500

    fix: Updates Task.jsx to use 'props'.

--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -27,3 +27,28 @@ Task.propTypes = {
 };
 
 export default Task;
+
+
+const Task = (props) => {
+  const [complete, setComplete] = useState(isComplete);
+  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+
+  return (
+    <li className="tasks__item">
+      <button
+        className={`tasks__item__toggle ${buttonClass}`}
+        onClick={() => setComplete(!complete)}
+      >
+        {props.title} 
+      </button>
+      <button className="tasks__item__remove button">x</button>
+    </li>
+  );
+};
+
+Task.propTypes = {
+  id: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  isComplete: PropTypes.bool.isRequired
+};
+export default Task;
